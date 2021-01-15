fd3d2a3d3eb999c0edb9a7d4c857755a67be0837
Star <46096192+norhu1130@users.noreply.github.com>
2021-01-15 16:11:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+/*
+WEB CORE
+해당 WEB CORE의 저작권은 norhu1130 본인이 소유하고있습니다.
+norhu1130의 허가없이 재배포등을 하실경우 불이익이 생길수 있습니다.
+*/
 const express = require('express');
 const app = express();
 const config = require('./config.json')
