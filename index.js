661e4379ef9b7e2953ac69255eef76aca6b969a8
norhu1130 <norhu1130@naver.com>
2021-01-17 18:34:38 +0900

    v0.0.1

--- index.js
+++ index.js
@@ -7,12 +7,25 @@ const express = require('express');
 const app = express();
 const config = require('./config.json')
 const port = config.port;
+var engines = require('consolidate');
+const createError = require('http-errors')
 var version = '0.0.1'
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
 app.use(express.static(__dirname + "/views"));
 app.use(express.static(__dirname + "/public"));
 app.use(function(req, res, next) {
-	res.status(404).send('404오류 찾지 못했습니다.');	
-})
+  next(createError(404));
+});
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('404.html');
+});
 app.listen(port, () => {
     console.log(`Server Started (PORT ${config.port})`);
     console.log(`Server Version ${version}`)
