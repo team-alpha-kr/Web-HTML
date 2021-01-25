0014a46ebffd27335635050db390eb49d2ce47f6
My Name <comjun04@gmail.com>
2021-01-25 22:13:32 +0900

    Fix critical bug

--- index.js
+++ index.js
@@ -8,24 +8,27 @@ WEB CORE
 norhu1130의 허가없이 재배포등을 하실경우 불이익이 생길수 있습니다.
 */
 
-const express = require('express');
-const app = express();
-const config = require('./config.json')
-const port = config.port;
-var engines = require('consolidate');
+const express = require('express')
+const app = express()
+
+const engines = require('consolidate')
 const createError = require('http-errors')
-let version = '0.0.2'
-const router = require('./router/main')(app);
 
-let port = 3000;
+const config = require('./config.json')
+const port = config.port || 3000
+const version = '0.0.2'
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
-app.engine('html', require('ejs').renderFile);
+const router = require('./router/main')
 
-app.use('/public', express.static('public'));
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+app.engine('html', require('ejs').renderFile)
 
-let server = app.listen(port, () => {
-  console.log(`Server Started (PORT ${config.port})`);
+app.use('/public', express.static('public'))
+
+app.use(router)
+
+app.listen(port, () => {
+  console.log(`Server Started (PORT ${config.port})`)
   console.log(`Server Version ${version}`)
 })
