683d7e8447274f792fe8ad598da0a19d2ce74aab
LINEOUS <juni37@outlook.kr>
2021-01-31 19:25:29 +0900

    ejs 패치(kms0219kms)

--- public/static/js/app.js
+++ public/static/js/app.js
@@ -1,27 +1,52 @@
-function includeHTML() {
-  var z, i, elmnt, file, xhttp;
-  /* Loop through a collection of all HTML elements: */
-  z = document.getElementsByTagName("*");
-  for (i = 0; i < z.length; i++) {
-    elmnt = z[i];
-    /*search for elements with a certain atrribute:*/
-    file = elmnt.getAttribute("include-html");
-    if (file) {
-      /* Make an HTTP request using the attribute value as the file name: */
-      xhttp = new XMLHttpRequest();
-      xhttp.onreadystatechange = function() {
-        if (this.readyState == 4) {
-          if (this.status == 200) {elmnt.innerHTML = this.responseText;}
-          if (this.status == 404) {elmnt.innerHTML = "Page not found.";}
-          /* Remove the attribute, and call this function once more: */
-          elmnt.removeAttribute("include-html");
-          includeHTML();
-        }
+(function(){
+
+  var frame = document.createElement('iframe');
+  frame.src = 'https://43vzwhsk2dvq.statuspage.io/embed/frame';
+  frame.style.position = 'fixed';
+  frame.style.border = 'none';
+  frame.style.boxShadow = '0 20px 32px -8px rgba(9,20,66,0.25)';
+  frame.style.zIndex = '9999';
+  frame.style.transition = 'left 1s ease, bottom 1s ease, right 1s ease';
+
+  var mobile;
+  if (mobile = screen.width < 450) {
+    frame.src += '?mobile=true';
+    frame.style.height = '20vh';
+    frame.style.width = '100vw';
+    frame.style.left = '-9999px';
+    frame.style.bottom = '-9999px';
+    frame.style.transition = 'bottom 1s ease';
+  } else {
+    frame.style.height = '115px';
+    frame.style.width = '320px';
+    frame.style.left = '-9999px';
+    frame.style.right = 'auto';
+    frame.style.bottom = '60px';
+  }
+
+  document.body.appendChild(frame);
+
+  var actions = {
+    showFrame: function() {
+      if (mobile) {
+        frame.style.left = '0';
+        frame.style.bottom = '0';
       }
-      xhttp.open("GET", file, true);
-      xhttp.send();
-      /* Exit the function: */
-      return;
+      else {
+        frame.style.left = '60px';
+        frame.style.right = 'auto'
+      }
+    },
+    dismissFrame: function(){
+      frame.style.left = '-9999px';
     }
   }
-}
+
+  window.addEventListener('message', function(event){
+    if (event.data.action && actions.hasOwnProperty(event.data.action)) {
+      actions[event.data.action](event.data);
+    }
+  }, false);
+
+  window.statusEmbedTest = actions.showFrame;
+})();
---
+++ run.bat
@@ -0,0 +1,4 @@
+@echo off
+title 메인 사이트 실행
+npm start
+pause >nul
