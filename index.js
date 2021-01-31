1750179d0a4adb403a3b06b8253c5c88156d3bfb
norhu1130 <norhu1130@naver.com>
2021-01-31 10:58:20 +0900

    Fixed

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.set('views', __dirname + '/views')
 app.set('view engine', 'ejs')
 
 app.use('/public', express.static('public'))
-
+app.use('*', express.static(__dirname + '/public'));
 app.use(router)
 
 const httpPort = 80
